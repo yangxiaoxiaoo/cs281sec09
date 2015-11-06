b6c902524ccf4a87c77be981cc48f2d7f017db12
yangxiaofeng <yangxiaoxiaoo@163.com>
2015-11-06 18:38:46 -0500

    SBM MCMC fitting added, naive bayesian converge slow

---
+++ SBM_fitting.py
@@ -0,0 +1,30 @@
+from graph_models import *
+import networkx
+import matplotlib.pyplot as plt
+import scipy
+import scipy.special
+import numpy as np
+
+G = networkx.read_edgelist("MontereyBay_list.txt")
+A = np.asarray(networkx.to_numpy_matrix(G))
+print A.shape
+
+'''
+example::
+A = np.asarray(np.matrix([[1., 1.], [1., 1.]]))
+print type(A)
+B = np.zeros((2,2))
+print type(B)
+'''
+
+R = 5
+b1 = 0.5
+b0 = 0.5
+a  = 1
+alpha0 = a*np.ones(R)
+model = StochasticBlockModel(R, b0, b1, alpha0)
+#starting with a set of prior. --may choose them to accelerate converging
+
+f_trace, theta_trace, lp_trace = fit_network(A, model, x0=None, N_iter=50, callback=None, pause=False)
+
+
