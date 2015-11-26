eb1a78d38c08ea2ad0b36effbc2b889a115364cc
yangxiaofeng <yangxiaoxiaoo@163.com>
2015-11-26 12:52:42 -0500

    run louvain clustering for all other graphs

--- single_cluster_clustering.py
+++ single_cluster_clustering.py
@@ -4,8 +4,12 @@
 import os
 import subprocess
 
-path = "/home/cbw/sbm/fb/"
-files = os.listdir(path)
+path = "/home/cbw/sbm/"
+#files = os.listdir(path)
+file_list = list()
+for root, dirs, files in os.walk(path, topdown=False):
+    for name in files:
+        file_list.append(os.path.join(root, name))
 
 
 processed = set()
@@ -13,7 +17,7 @@ queue_this_time = list()
 with open("blocksizes.txt", 'r')as frefer:
     for line in frefer:
         processed.add(line.split(' ')[0])
-for f in files:
+for f in file_list:
     if f.strip('.lgl') not in processed:
         if f.split('.')[1] == 'lgl':
             queue_this_time.append(f)
