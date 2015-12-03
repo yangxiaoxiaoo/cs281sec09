c6a1907b7a2aeaeff329d1853711397861323aca
yangxiaofeng <yangxiaoxiaoo@163.com>
2015-12-03 01:16:22 -0500

    redo louvain to store density and content of each cluster

--- Graph_louvain.py
+++ Graph_louvain.py
@@ -3,11 +3,11 @@ import sys
 import networkx
 
 
+'''
+new usage:
+python Graph_louvain.py "/net/data/graph-models/realgraph/Chicago_2008.ncol"
+'''
 
-'''
-usage: python Graph_louvain.py /home/cbw/sbm/fb/Egypy_2008.lgl
-will append result into output file "blocksizes.txt"
-'''
 
 
 
@@ -41,11 +41,39 @@ def decide_block_num(G,name):
 
 
 def partition(graphname):
+    '''
+    old,
+usage: python Graph_louvain.py /home/cbw/sbm/fb/Egypy_2008.lgl
+will append result into output file "blocksizes.txt"
+'''
     shortname = graphname.split('/')[-1].strip('.lgl')
     convert(graphname, "/home/xiaofeng/facebook/sparsify/cs281sec09/" + shortname + ".ncol")
     decide_block_num(networkx.read_edgelist(shortname + ".ncol"), shortname)
 
+def main(graph):
+    '''
+    new,
+will append result into output file "blocksizes_new.txt"
+and record for each graph a file in "nnum-enum-nlist" with
+cluster index, cluster node numbers, cluster edge numbers, who is in cluster.
+'''
+    shortname = str(graph.split('/')[-1].strip('.ncol'))
+    partition = community.best_partition(graph)
+    sizes = list()
+    for com in set(partition.values()):
+        count = count + 1.
+        list_nodes = [nodes for nodes in partition.keys() if partition[nodes] == com]
+        size_com = len(list_nodes)
+        sizes.append(size_com)
+        numedges = networkx.number_of_edges(graph.subgraph(list_nodes))
+        with open("/net/data/graph-models/louvain-clusters/nnum-enum-nlist/" + shortname +".density", 'a') as fout1:
+            fout1.write(str(count) +' ' + str(size_com) + ' ' + str(numedges) + str(list_nodes) + '\n')
+    sizes.sort()
+    with open("/net/data/graph-models/louvain-clusters/blocksizes_new.txt", 'a') as fout:
+        fout.write( shortname + ' ' + str(sizes)+'\n')
+
 
 if __name__ == "__main__":
-    partition(sys.argv[1])
- #   main()
+    #partition(sys.argv[1])
+
+    main(sys.argv[1])
--- single_cluster_clustering.py
+++ single_cluster_clustering.py
@@ -1,30 +1,50 @@
-'''community not installed on other clusters!
-for all facebook graphs, do louvain clustering, white cluster size into an output file "blocksizes.txt"
+'''
+for all facebook graphs, do louvain clustering
 '''
 
 import os
 import subprocess
 
-path = "/home/cbw/sbm/"
-#files = os.listdir(path)
-file_list = list()
-for root, dirs, files in os.walk(path, topdown=False):
-    for name in files:
-        file_list.append(os.path.join(root, name))
+def initial_transformation():
+    #used once for transforming lgl into edgelists
+    #write cluster size into an output file "blocksizes.txt"
 
 
-processed = set()
-queue_this_time = list()
-with open("blocksizes.txt", 'r')as frefer:
-    for line in frefer:
-        processed.add(line.split(' ')[0])
-for f in file_list:
-    if f.strip('.lgl') not in processed:
-        if len(f.split('.')) == 2 and f.split('.')[1] == 'lgl':
-            queue_this_time.append(f)
+    path = "/home/cbw/sbm/"
+    #files = os.listdir(path)
+    file_list = list()
+    for root, dirs, files in os.walk(path, topdown=False):
+        for name in files:
+            file_list.append(os.path.join(root, name))
 
-for i in range(0, len(queue_this_time)):
-    graphfile = os.path.join(path, queue_this_time[i])
-    subprocess.check_call(['python',
-         '/home/xiaofeng/facebook/sparsify/cs281sec09/Graph_louvain.py',
-         graphfile])
+
+    processed = set()
+    queue_this_time = list()
+    with open("blocksizes.txt", 'r')as frefer:
+        for line in frefer:
+            processed.add(line.split(' ')[0])
+    for f in file_list:
+        if f.strip('.lgl') not in processed:
+            if len(f.split('.')) == 2 and f.split('.')[1] == 'lgl':
+                queue_this_time.append(f)
+
+    for i in range(0, len(queue_this_time)):
+        graphfile = os.path.join(path, queue_this_time[i])
+        subprocess.check_call(['python',
+            '/home/xiaofeng/facebook/sparsify/cs281sec09/Graph_louvain.py',
+            graphfile])
+
+def main():
+    #after having "graph-models" directory
+    #store cluster result into /net/data/graph-models/louvain-clusters
+
+    path = "/net/data/graph-models/realgraph"
+    for file in os.listdir(path):
+        subprocess.check_call(['python',
+            '/home/xiaofeng/facebook/sparsify/cs281sec09/Graph_louvain.py',
+            os.path.join(path,file)])
+
+
+
+if __name__ == "__main__":
+    main()
