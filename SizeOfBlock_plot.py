a8a2497fdb1743e784e8089b51190526655ea58d
yangxiaofeng <yangxiaoxiaoo@163.com>
2015-12-04 13:12:43 -0500

    90 coverage

--- SizeOfBlock_plot.py
+++ SizeOfBlock_plot.py
@@ -21,7 +21,7 @@ fout = "/net/data/graph-models/louvain-clusters/blocknum_threshold_90.txt"
 with open(fin, 'r') as blockfile:
     for line in blockfile:
         name = line.split(' ')[0]
-        large_count = 0
+        large_count = 1
         values = line.strip(name + ' ').strip('[').strip(']\n').split(',')
         node_num = sum(map(lambda x: int(x), values))
         max_cluster = int(values[-1])
