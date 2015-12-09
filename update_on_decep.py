0d445e5e70771a63e6dcb2b57686d8dd99fb85c4
yangxiaofeng <yangxiaoxiaoo@163.com>
2015-12-09 15:40:27 -0500

    SBM fitting result checking

--- update_on_decep.py
+++ update_on_decep.py
@@ -1,5 +1,7 @@
 '''
 usage: python update_on_decep.py graph_models.py
+for uppdating code on decepticons
+alter_usage: reading the otput file of SBM_fitting and see how many of them finished
 '''
 
 import sys
@@ -19,6 +21,20 @@ def main(file_name):
             shell=True)
 
 
+def check_fit_results():
+    machines = ['bonecrusher',
+                'hook',
+                'longhaul',
+                'mixmaster',
+                'scrapper',
+                'scavenger'
+                ]
+    for machine in machines:
+        subprocess.check_call(
+            "ssh " + machine + "wc -l ~/graph-models/SBM_fit_all_3iter.txt",
+            shell=True)
+
 
 if __name__ == "__main__":
-    main(sys.argv[1])
+   # main(sys.argv[1])
+    check_fit_results()
