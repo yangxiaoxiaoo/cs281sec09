28e2c5a0384a30df9c9f02ced131edb378967d59
yangxiaofeng <yangxiaoxiaoo@163.com>
2015-12-13 21:31:20 -0500

    louvain based p-decision, and then plot

--- derectly_decide.py
+++ derectly_decide.py
@@ -4,27 +4,38 @@ write result into /net/data/graph-models/louvain-clusters/block_paras
 and plot p to size of blocks for these large blocks
 '''
 
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
 
 
 def main():
-
+    bigger_node_sizes = list()
+    p_values = list()
     with open("/net/data/graph-models/louvain-clusters/blocknum_threshold_90.txt", 'r') as fin:
-        bigger_node_sizes = list()
-        p_values = list()
         for line in fin:
             name = line.split(' ')[0]
             print name
             min_size_interested = int(line.split(' ')[2])
             with open("/net/data/graph-models/louvain-clusters/nnum-enum-nlist/" + name + ".density", 'r') as fin2:
-                for line in fin2:
-                    node_num = int(line.split(' ')[1])
-                    edge_num = int(line.split(' ')[2].split('[')[0])
-                    if node_num > min_size_interested:
-                        complete_num = node_num * (node_num - 1) / 2
-                        p = float(edge_num) / float(complete_num)
-                        print p
-                        bigger_node_sizes.append(node_num)
-                        p_values.append(p)
+                with open("/net/data/graph-models/louvain-clusters/block_paras/" + name + ".pvalue", 'w') as fout:
+                    for line in fin2:
+                        node_num = int(line.split(' ')[1])
+                        edge_num = int(line.split(' ')[2].split('[')[0])
+                        if node_num > min_size_interested:
+                            complete_num = node_num * (node_num - 1) / 2
+                            p = float(edge_num) / float(complete_num)
+                            fout.write(str(node_num) + ' ' + str(p) + '\n')
+                            bigger_node_sizes.append(node_num)
+                            p_values.append(p)
+
+    plt.scatter(bigger_node_sizes, p_values,color='red')
+    plt.yscale('log')
+    plt.ylabel('p-value')
+    plt.xscale('log')
+    plt.xlabel('number of nodes')
+    plt.savefig("plot_pvalue.pdf", facecolor='w', edgecolor='w',orientation='portrait')
+
 
 if __name__ == "__main__":
     main()
