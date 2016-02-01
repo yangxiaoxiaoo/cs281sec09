831e0b97d250c10e23300dc35d03df53987259e4
yangxiaofeng <yangxiaoxiaoo@163.com>
2016-02-01 16:21:24 -0500

    tested auto generation

--- Auto_simulation.py
+++ Auto_simulation.py
@@ -3,6 +3,9 @@ import os
 import matplotlib.pyplot as plt
 import networkx as nx
 import itertools
+from networkx.readwrite import json_graph
+import json
+
 
 FIG_FOLDER = "./figs/"
 
@@ -206,6 +209,7 @@ def motif_H():
 def deisomorphism(patternset):
     deisoed_set = set()
     for item in patternset:
+        print item
         if any(nx.is_isomorphic(item, item2) for item2 in deisoed_set):
             pass
         else:
@@ -221,34 +225,51 @@ def merge_nodes(G,node1,node2):
 def patternsets2(MotifG1, MotifG2):
     #enumerate all possible permutations of node labels,
     #minimum is sharing one edge, all the way to max is the smaller number of edges, complexity 2^edgenum_max
-    #return a set
+    #return a set of possibly isomorphic collapses
 
     patternset = set()
     edgenum_max = min(MotifG1.number_of_edges(), MotifG2.number_of_edges())
 
-    #one edge overlap case
-    for edge in MotifG1.edges():
-        s = edge[0]
-        t = edge[1]
-        for edge2 in MotifG2.edges():
-            #union requires the two Graph has no common nodes
-            new_collapse = nx.union(MotifG1, MotifG2)
-
-
     #select L (two+) edges to overlap
-    for L in range(0, edgenum_max + 1):
-        mapping1 = dict()
-        mapping2 = dict()
-        for subset1 in itertools.combinations(MotifG1.edges(),L):
-            for subset2 in itertools.combinations(MotifG2.edges(),L):
-                permutation1 = list(itertools.permutations(subset1)).pop()
-                permutation2 = list(itertools.permutations(subset2)).pop()
-                for edge1 in permutation1:
-                    for edge2 in permutation2:
+    for L in range(1, edgenum_max + 1):
+        L_subsets = list(itertools.combinations(MotifG1.edges(),L))
+        L_subsets2 = list(itertools.combinations(MotifG2.edges(),L))
+     #   print "all possible combination of choosing"+str(L) +"edges"
+     #   print L_subsets2
+        for subset1 in L_subsets:
+            for subset2 in L_subsets2:
+             #   print "already chose these" +str(L)+" edges in Motif2"
+             #   print subset2
+                permutations = list(itertools.permutations(subset1))
+             #   print "already chose another"+str(L)+"in Motif1, now ordering them"
+             #   print permutations
+                i = 0
+                for permutation in permutations:
+               #     print "this permutation is"
+               #     print permutation
+               #     print "in this particular order" + str(i)
+                    for j in range(0, len(permutation)):
+                        edge_1 = permutation[j]
+                        edge_2 = subset2[j]
+                #        print "edge 1"
+                #        print edge_1
+                #        print "edge 2"
+                #        print edge_2
+                    i += 1
+                    if MotifG1 == MotifG2:
+                        print "!!same motif"
+                    else:
+                        G = nx.union(MotifG1, MotifG2)
+                    G1 = merge_nodes(G, edge_1[0], edge_2[0])
+                    G2 = merge_nodes(G1, edge_1[1], edge_2[1])
+                    patternset.add(G2)
 
+                    G = nx.union(MotifG1, MotifG2)
+                    G11 = merge_nodes(G, edge_1[1], edge_2[0])
+                    G22 = merge_nodes(G11, edge_1[0], edge_2[1])
+                    patternset.add(G22)
 
-
-
+    return patternset
 
 
 def enumerateall():
@@ -268,8 +289,10 @@ def enumerateall():
     Motifset = set([G_1a, G_1b])
     for Motif1 in Motifset:
         for Motif2 in Motifset:
-            allpatterns = allpatterns.union(patternsets2(Motif1, Motif2))
-
+            if not Motif1 == Motif2:
+                for item in patternsets2(Motif1, Motif2):
+                    print item
+                    allpatterns.add(item)
     return deisomorphism(allpatterns)
 
 def test():
@@ -277,8 +300,17 @@ def test():
     G_1a.add_edge(1, 2)
     G_1a.add_edge(2, 3)
     print (len(merge_nodes(G_1a,1, 3)) == 2)
+    G_2 = nx.Graph()
+    G_2.add_edge(3, 2)
+    test_set = set()
+    test_set.add(G_1a)
+    test_set.add(G_2)
+    print nx.is_isomorphic(G_1a, G_2)
+    print nx.could_be_isomorphic(G_1a, G_2)
+    print deisomorphism(test_set)
+
 
 if __name__ == '__main__':
 
-    #enumerateall()
-    test()
+    print len(enumerateall())
+    #test()
