1973ca319214cfd7377586198901c3b11e36699d
yangxiaofeng <yangxiaoxiaoo@163.com>
2016-02-23 01:20:07 -0500

    reformat community files

--- reformat_communities.py
+++ reformat_communities.py
@@ -6,7 +6,7 @@ if __name__ == "__main__":
     filepath = "/net/data/graph-models/louvain-clusters/communities/"
     for file in os.listdir(filepath):
         fin = open(os.path.join(filepath, file),"r")
-        fout = open(os.path.join(filepath, file, ".new"), "w")
+        fout = open(os.path.join(filepath, file + ".new"), "w")
         for line in fin:
             fout.write(line.replace("{}", "1"))
         fin.close()
