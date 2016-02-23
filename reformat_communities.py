3e0cdb06cef3023a0ec51c99f246c0c482354a2d
yangxiaofeng <yangxiaoxiaoo@163.com>
2016-02-23 01:18:24 -0500

    reformat community files

---
+++ reformat_communities.py
@@ -0,0 +1,13 @@
+#reformat communities edges
+
+import os
+
+if __name__ == "__main__":
+    filepath = "/net/data/graph-models/louvain-clusters/communities/"
+    for file in os.listdir(filepath):
+        fin = open(os.path.join(filepath, file),"r")
+        fout = open(os.path.join(filepath, file, ".new"), "w")
+        for line in fin:
+            fout.write(line.replace("{}", "1"))
+        fin.close()
+        fout.close()
