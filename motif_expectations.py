e9cc5e2a7c5b29c0ec780f6f71c152d89992c7cd
yangxiaofeng <yangxiaoxiaoo@163.com>
2016-03-14 20:23:06 -0400

    move to 3-approximations

--- motif_expectations.py
+++ motif_expectations.py
@@ -1,6 +1,118 @@
 #calculate an stimation occrance for all shapes in 5
 import matplotlib.pyplot as plt
 import numpy as np
+import networkx
+import json
+from networkx.readwrite import json_graph
+import subprocess
+
+def hop2(G, A):
+    hop2_set = set()
+    for neighbor in G.neighbors(A):
+        hop2_set.add(neighbor)
+        for neighbor2 in G.neighbors(neighbor):
+            hop2_set.add(neighbor2)
+    return hop2_set
+
+def hop3(G, A):
+    hop3_set = set()
+    for neighbor in G.neighbors(A):
+        hop3_set.add(neighbor)
+        hop3_set = hop3_set.union(hop2(G, neighbor))
+    return hop3_set
+
+def edge_to_cost(edge, G_string):
+    dataG = json.loads(G_string)
+    G = json_graph.node_link_graph(dataG)
+    A = int(edge[0])
+    B = int(edge[1])
+    edge = (A, B)
+    local_set = hop2(G, A).union(hop2(G, B))
+    local_set.add(A)
+    local_set.add(B)
+    interested = G.subgraph(local_set)
+    hop3_before = 0
+    for node in interested.nodes():
+        hop3_before += len(hop3(interested, node))
+    interested.remove_edge(*edge)
+    hop3_after = 0
+    for node in interested.nodes():
+        hop3_after += len(hop3(interested, node))
+    loss = (hop3_before - hop3_after)/2
+    return edge, loss
+
+def G_string_to_safe_totalcost(G_string):
+    safe_edges = 0
+    total_cost = 0
+    dataG = json.loads(G_string)
+    G = json_graph.node_link_graph(dataG)
+    for edge in G.edges():
+        loss = edge_to_cost(edge, G_string)[1]
+        total_cost += loss
+        if loss == 0:
+            safe_edges += 1
+    return safe_edges, total_cost
+
+def choose(n, k):
+    if 0 <= k <= n:
+        ntok = 1
+        ktok = 1
+        for t in xrange(1, min(k, n - k) + 1):
+            ntok *= n
+            ktok *= t
+            n -= 1
+        return ntok // ktok
+    else:
+        return 0
+
+def exp5_3(n, p):
+    #how larger motifs affect the edges that can be safely removed
+    fin = open('/net/data/graph-models/sim-graphs/approx5-unfiltered', 'r')
+    error5_3 = 0 #how many edges can be safely removed
+    OF5_3 = 0 #how many 3-hop relations are there in the larger motif
+
+    for line in fin:
+        data = json.loads(line)
+        graph1 = json_graph.node_link_graph(data)
+        num_n = graph1.number_of_nodes()
+        num_e = graph1.number_of_edges()
+        N_graph_exp = choose(n, num_n) * pow(p, num_e) * pow((1-p), (num_n * (num_n - 1)/2 - num_e))
+
+
+        error_multiplyer =
+        error5_3 += error_multiplyer * N_graph_exp
+    return error5_3, OF5_3
+
+
+
+
+def OF5(n, p):
+    #objective function in 5 -approximation
+    m = p * n *(n-1)/2
+
+    N_3line = 12 * choose(n, 4) * pow(p,3) * pow((1-p),3)
+    N_2line = 3 * choose(n, 3) * pow(p,2) * (1-p)
+    N_1a = 12 * choose(n, 4) * pow(p,4) * pow((1-p),2)
+    N_1b = 3 * choose(n, 3) * pow(p,3)
+    N_2a = 60 * choose(n, 5) * pow(p,5) * pow((1-p),5)
+    N_2b = 12 * choose(n, 4) * pow(p,4) * pow((1-p),2)
+    N_2c = 24 * choose(n, 4) * pow(p,4) * pow((1-p),2)
+    N_3 = 360 * choose(n, 6) * pow(p,6) * pow((1-p),9)
+
+    Error2_3 = (N_1a * N_1b + N_1a * N_2a + N_1a * N_2b + N_1a * N_2c + N_1a* N_3
+                            + N_1b * N_2a + N_1b * N_2b + N_1b * N_2c + N_1b* N_3
+                                          + N_2a * N_2b + N_2a * N_2c + N_2a* N_3
+                                                        + N_2b * N_2c + N_2b* N_3
+                                                                      + N_2c* N_3
+                + pow(N_1a, 2) + pow(N_1b, 2) + pow(N_2a, 2) + pow(N_2b, 2) + pow(N_2c, 2) + pow(N_3, 2)
+            )/m
+
+    Appx1 = m + N_3line + N_2line
+    Error1_2 = N_1a + N_1b + N_2a + N_2b + N_2c + N_3
+    Appx2 = Appx1 - Error1_2
+    Appx3 = Appx2 + Error2_3
+    return Appx3
+
 
 def oracle_5(n, p):
     edge_num_list = list()
@@ -27,22 +139,15 @@ def oracle_5(n, p):
                            )/m
     edge_num_list.append(x_1)
     OF_list.append(0)
-    #p2
-    #x_2 = x_1 + 3*N_3line - 6*N_3 - 5 * N_2a - 4 * N_1a
-    #edge_num_list.append(x_2)
-    #OF_list.append(OF3(n, p) - dis2_relations(n,p))
-    #p3
-    #x_3 = x_2 + 2*N_2line - 4* N_1b - 4* N_2b - 4*N_2c
-    #edge_num_list.append(x_3)
-    #OF_list.append(OF3(n, p) - (m - N_3line/12 - N_2line/3))
-    #p4
+
     edge_num_list.append(m)
     OF_list.append(OF5(n, p))
 
     return edge_num_list, OF_list
 
 
-
+def main():
+    subprocess.check_call("hdfs dfs -get patterns_queue1 /net/data/graph-models/sim-graphs/approx5-unfiltered")
 
 
 
