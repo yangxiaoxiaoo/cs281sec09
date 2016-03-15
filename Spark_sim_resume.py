fd4a5ae2169f3a3afaf0eeee159c7a62a4f9a7b9
yangxiaofeng <yangxiaoxiaoo@163.com>
2016-03-15 13:59:50 -0400

    format for 5-approx

---
+++ Spark_sim_resume.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python
+import subprocess
+from pyspark import SparkContext
+from pyspark.streaming import StreamingContext
+import json
+import pickle
+from networkx.readwrite import json_graph
+import networkx as nx
+import itertools
+
+
+
+def main():
+
+    sc = SparkContext(appName="Motif_counting")
+    collapsed_patterns = sc.textFile("hdfs://scrapper/user/xiaofeng/patterns_queue1")
+    output_file_inter = "/net/data/graph-models/sim-graphs/approx5-json-inter"
+    non_iso_set = set()
+
+    def iso_json(string1,string2):
+        dataG1 = json.loads(string1)
+        graph1 = json_graph.node_link_graph(dataG1)
+        dataG2 = json.loads(string2)
+        graph2 = json_graph.node_link_graph(dataG2)
+       # return nx.is_isomorphic(graph1, graph2)
+        return nx.faster_could_be_isomorphic(graph1, graph2)
+
+
+    while True:
+        left_size = collapsed_patterns.count()
+        print "left RDD size to be processed:"
+        print left_size
+        if left_size <= 1:
+            return 0
+        povet = collapsed_patterns.take(1)[0]#BROADCAST
+        povet_broad = sc.broadcast(povet)
+        non_iso_set.add(povet)
+        fout_inter = open(output_file_inter, 'a')
+        fout_inter.write(str(left_size) + ' ' + povet + '\n')
+        fout_inter.close()
+        collapsed_patterns = collapsed_patterns.filter(lambda x: not iso_json(x, povet_broad.value))
+
+
+
+if __name__ == "__main__":
+    main()
