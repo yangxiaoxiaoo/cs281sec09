800b46260b47810fab81a6e0918c7bcc8994563d
yangxiaofeng <yangxiaoxiaoo@163.com>
2016-03-25 01:28:10 -0400

    mfinder automated to 6

--- para_subraph_find.py
+++ para_subraph_find.py
@@ -69,7 +69,7 @@ def main():
 
     machines = ["achtung%02i" % (x) for x in range(2, 12)]
 
-    m_size = 5
+    m_size = 6
     while m_size <= 6:
         graph_ids = get_graph_ids(m_size)
         print "graph ids"
