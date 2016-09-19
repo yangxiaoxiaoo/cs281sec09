562a3a060dc7aca055c0f312bef9210431d119e8
yangxiaofeng <yangxiaoxiaoo@163.com>
2016-09-18 23:21:16 -0400

    restarted experiments

---
+++ Realquery_sim.py
@@ -0,0 +1,34 @@
+from graph_models import *
+import networkx
+import time
+import random
+import motif_expectations
+
+def querytime(Graphname):
+    ts1 = time.time()
+    G = networkx.read_edgelist("/net/data/graph-models/realgraph/" + Graphname + ".ncol")
+    seednode = random.choice(G.nodes)
+    print Graphname + " query "+str(seednode) +"is: "
+    print len(motif_expectations.hop3(G, seednode))
+    ts2 = time.time()
+    t_interval = ts2 - ts1
+    print "Time it takes is " + str(t_interval)
+    return  t_interval
+
+def creatlist():
+    fin = open("blocknumbers_auto200.txt", 'r')
+    Graphnames = list()
+    for line in fin:
+        Graphname = line.split(' ')[0]
+        Graphnames.append(Graphname)
+    return Graphnames
+
+
+def main():
+    graphlist = creatlist()
+    for graphname in graphlist:
+        time = querytime(graphname)
+
+if __name__ == "__main__":
+    main()
+
